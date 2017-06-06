6cbddba3298ad84f3a01f899972d806cc7b28ebd
julse <julse@qq.com>
2017-06-06 21:10:34 +0800

    完善Utility,解析服务器返回的json数据

--- src/util/Utility.java
+++ src/util/Utility.java
@@ -1,5 +1,9 @@
 package util;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
 import android.text.TextUtils;
 import model.City;
 import model.County;
@@ -70,5 +74,40 @@ public class Utility {
 		}
 		return false;
 	}
-	
+	/**
+	 * 解析服务器返回的JSON数据，并将解析出的数据存储到本地
+	 */
+	public static void handleWeatherResponse(Context context,String response){
+		try {
+			JSONObject jsonObject = new JSONObject(response);
+			JSONObject weatherInfo=jsonObject.getJSONObject("weatherinfo");
+			String cityName = weatherInfo.getString("city");
+			String weatherCode = weatherInfo.getString("cityid");
+			String temp1 = weatherInfo.getString("temp1");
+			String temp2 = weatherInfo.getString("temp2");
+			String weatherDesp=weatherInfo.getString("weather");
+			String publishTime = weatherInfo.getString("ptime");
+			saveWeatherInfo(context,cityName,weatherCode,temp1,temp2,weatherDesp,publishTime);
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * 将服务器返回的所有天气信息存储到SharedPreferences文件中
+	 * @param context
+	 * @param cityName
+	 * @param weatherCode
+	 * @param temp1
+	 * @param temp2
+	 * @param weatherDesp
+	 * @param publishTime
+	 */
+	private static void saveWeatherInfo(Context context, String cityName,
+			String weatherCode, String temp1, String temp2, String weatherDesp,
+			String publishTime) {
+//		写到这里
+		
+	}
 }
---
+++ src/util/WeatherType.java
@@ -0,0 +1,13 @@
+package util;
+
+public interface WeatherType {
+//	public final static String WEATHERINFO="weatherInfo";
+	public final static String CITY_SELECTED="city_selected";
+	public final static String CITY_NAME="city_name";
+	public final static String WEATHER_CODE="weather_code";
+	public final static String TEMP1="temp1";
+	public final static String TEMP2="temp2";
+	public final static String WEATHER_DESP="weather_desp";
+	public final static String PUBLISH_TIME="publish_time";
+	public final static String CURRENT_DATE="current_date";
+}
